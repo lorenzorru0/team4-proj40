01bc62786385f718c34f49552c7eed8d202c1c96
lorenzorru0 <lorenzoorru999@gmail.com>
2021-12-02 16:30:05 +0100

    Add types migration

---
+++ app/Type.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use App\User;
+
+class Type extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'slug',
+    ];
+
+    public function users() 
+    {
+        return $this->belongsToMany('App\User');
+    }
+}

538b6815aad1dd59e3e68718205b732c6e2b2fac
lorenzorru0 <lorenzoorru999@gmail.com>
2021-12-03 11:45:44 +0100

    Add photo import

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,6 +15,8 @@
                     @endif
 
                     {{ __('You are logged in!') }}
+
+                    <img class="img-fluic" src="{{asset('storage/'. Auth::user()->url_cover)}}" alt="Cover user">
                 </div>
             </div>
         </div>

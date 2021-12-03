f4e5566b17024d29c53252376335340868174ea9
touros91 <andreageraci91@gmail.com>
2021-12-03 12:17:49 +0100

    Add restaurant details on dashboard

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,5 +1,12 @@
 @extends('layouts.app')
 
 @section('pageContent')
-    <h2>ciao</h2>
+<h2>Dettagli Ristorante</h2>
+
+    <ul>
+        <li>Nome Ristorante: {{$user->business_name}}</li>
+        <li>Indirizzo: {{$user->address}}</li>
+        <li>Partita IVA: {{$user->vat_number}}</li>
+    </ul>
+
 @endsection

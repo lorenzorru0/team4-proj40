fe746fc92aed2fc555624a63ae6061d4c765df03
touros91 <andreageraci91@gmail.com>
2021-12-03 11:41:30 +0100

    Add UserController e rotte Admin

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,11 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
+
+// Rotte area Admin
+Route::middleware('auth')->namespace('Admin')->name('admin.')->prefix('admin')->group(function() {
+    // Route::get('/', 'HomeController@index')->name('home');
+    Route::resource('/', 'UserController');
+
+});

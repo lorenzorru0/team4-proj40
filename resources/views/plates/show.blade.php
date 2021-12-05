efdcba4d469fde45198f93ee098b29a843eced1e
touros91 <andreageraci91@gmail.com>
2021-12-05 14:36:23 +0100

    Finish Show view

--- resources/views/plates/show.blade.php
+++ resources/views/plates/show.blade.php
@@ -4,9 +4,19 @@
 
 <div class="container">
     <ul>
-        <li>{{$plate->plate_name}}</li>
+        <li>Nome del piatto: <strong>{{$plate->plate_name}}</strong></li>
+        <li>Prezzo: Є{{$plate->price}}</li>
+        @if ($plate->description)
+            <li>Descrizione: {{$plate->description}}</li>
+        @endif
+        @if ($plate->cooking_time)
+            <li>Tempo cottura: {{$plate->cooking_time}}</li>
+        @endif
     </ul>
-    <img src="{{asset('storage/'. $plate->url_photo)}}" alt="Plate photo">
+    
+    @if ($plate->url_photo)
+        <img src="{{asset('storage/'. $plate->url_photo)}}" alt="Plate photo">
+    @endif
 </div>
 
 @endsection

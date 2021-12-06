e78d41b4ef494fec072390837349e9a592fa45f1
touros91 <andreageraci91@gmail.com>
2021-12-06 16:37:07 +0100

    Fix Login buttons

--- resources/views/front/vuejs.blade.php
+++ resources/views/front/vuejs.blade.php
@@ -8,19 +8,22 @@
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
+    <div class="logins">
     @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ route('admin.users.index') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+        <div class="top-right links">
+            @auth
+                <a href="{{ route('admin.users.index') }}">Home</a>
+            @else
+                <a href="{{ route('login') }}">Login</a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}">Register</a>
+                @endif
+            @endauth
+        </div>
+    @endif
+    </div>
+
     <div id="app">
         
     </div>

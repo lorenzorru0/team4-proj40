501c50d1336aa80314d4d86a912a564dc0630746
touros91 <andreageraci91@gmail.com>
2021-12-17 12:23:42 +0100

    Add checkout page loader

--- resources/views/successfulPayment.blade.php
+++ resources/views/successfulPayment.blade.php
@@ -5,9 +5,83 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Deliveboo</title>
-    <link rel="shortcut icon" href="{{asset('images/favicon.ico')}}" type="image/x-icon">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+/* Center the loader */
+#loader {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  z-index: 1;
+  width: 120px;
+  height: 120px;
+  margin: -76px 0 0 -76px;
+  border: 16px solid #f3f3f3;
+  border-radius: 50%;
+  border-top: 16px solid #3498db;
+  -webkit-animation: spin 2s linear infinite;
+  animation: spin 2s linear infinite;
+}
+
+@-webkit-keyframes spin {
+  0% { -webkit-transform: rotate(0deg); }
+  100% { -webkit-transform: rotate(360deg); }
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+
+/* Add animation to "page content" */
+.animate-bottom {
+  position: relative;
+  -webkit-animation-name: animatebottom;
+  -webkit-animation-duration: 1s;
+  animation-name: animatebottom;
+  animation-duration: 1s
+}
+
+@-webkit-keyframes animatebottom {
+  from { bottom:-100px; opacity:0 } 
+  to { bottom:0px; opacity:1 }
+}
+
+@keyframes animatebottom { 
+  from{ bottom:-100px; opacity:0 } 
+  to{ bottom:0; opacity:1 }
+}
+
+#myDiv {
+  display: none;
+  text-align: center;
+}
+</style>
 </head>
-<body>
-    <h1>Il tuo pagamento è andato a buon fine! Grazie per aver scelto Deliveboo!</h1>
+<body onload="myFunction()" style="margin:0; height: 100vh; background: rgb(34,193,195); background: linear-gradient(127deg, rgba(34,193,195,1) 50%, rgba(255,255,255,1) 100%);">
+
+<div id="loader"></div>
+
+<div style="display:none; color: #440063; font-family:Arial, Helvetica, sans-serif" id="myDiv" class="animate-bottom">
+  <h1>Pagamento andato a buon fine</h1>
+  <h2>Grazie per aver scelto Deliveboo!</h2>
+  <div>
+    <a style="text-decoration: none; color: white; font-size: 20px" href="/">Torna all'Homepage</a>
+</div>
+</div>
+
+<script>
+var myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
+</script>
+
 </body>
 </html>

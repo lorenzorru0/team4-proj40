10a4642f4b28d56cb9517e110f46281fdc5531fe
Mikele91 <michele.amoruso91.8@gmail.com>
2021-12-20 19:32:27 +0100

    qualche aggiustata al backoffice

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -9,9 +9,9 @@
         <div class="row my-5">
             <div class="col-6">
 
-            <img class="mb-3 rounded" src="{{asset('storage/' . $user->url_cover)}}" alt="Cover photo ristorante">
+            <img class="logo mb-3 rounded" src="{{asset('storage/' . $user->url_cover)}}" alt="Cover photo ristorante">
             </div>
-            <div class="col-6">
+            <div class="d-flex flex-column justify-content-center align-items-center col-6">
                 <p>Nome Ristorante: <strong>{{$user->business_name}}</strong></p>
                 <p>Indirizzo: <strong>{{$user->address}} {{$user->street_number}}</strong></p>
                 <p>Partita IVA: <strong>{{$user->vat_number}}</strong></p>
@@ -23,18 +23,22 @@
         <p>Nessuna foto è stata inserita.</p>
     @endif
 
-    <div class="row">
+    <div class="row my-5">
        
-        <div class="col-6">
-            Tipologie collegate:
-            <ol>
+        <div class="list col-sm-12 col-md-6 ">
+            Tipologie:
+            <div>
                 @foreach ($user->types as $type)
-                    <li><strong>{{$type->name}}</strong></li>
+                    <span> {{$type->name}} -</span>
                 @endforeach
-            </ol>
+            </div>
+        </div>
+        
+        <div class="list col-sm-12 col-md-6 text-center my-2">
+            <img class="logo"  src="/images/logo_green.png" alt="">
         </div>
     </div>
-</div>
 
 @endsection
+
 
